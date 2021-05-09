31f6e1140a18f29e40a5b139a37d9f1744461afa
Akos Fi <fi.akos@straxus.hu>
2021-05-09 14:56:03 +0200

    [feature] CodeBuild project added

--- scripts/deploy-1-common.sh
+++ scripts/deploy-1-common.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-while getopts ":k:s:b:c:" opt; do
+while getopts ":k:s:b:c:u:p:" opt; do
   case $opt in
     k) AWS_ACCESS_KEY="$OPTARG"
     ;;
@@ -10,6 +10,10 @@ while getopts ":k:s:b:c:" opt; do
     b) AWS_S3_BUCKET_NAME="$OPTARG"
     ;;
     c) CLOUDFORMATION_STACK_NAME="$OPTARG"
+    ;;
+    u) DOCKERHUB_USERNAME="$OPTARG"
+    ;;
+    p) DOCKERHUB_PASSWORD="$OPTARG"
     ;;
     \?) echo "Invalid option -$OPTARG" >&2
     ;;
@@ -23,8 +27,10 @@ done
 
 ./scripts/helpers/s3_template_upload.sh -t "security-group/security-groups.yaml"
 
+./scripts/helpers/s3_template_upload.sh -t "codebuild/codebuild-project-storefront.yaml"
 
 aws cloudformation deploy \
     --template-file ./deployments/1-common.yaml \
     --stack-name "$CLOUDFORMATION_STACK_NAME" \
+    --parameter-overrides DockerHubUsername="$DOCKERHUB_USERNAME" DockerHubPassword="$DOCKERHUB_PASSWORD" \
     --capabilities CAPABILITY_NAMED_IAM
