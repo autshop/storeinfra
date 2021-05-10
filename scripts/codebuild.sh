63b0c3efca90481b1eacab68d78b301e02cbfe3c
Akos Fi <fi.akos@straxus.hu>
2021-05-10 09:27:31 +0200

    [feature] Fix codebuild script.

--- scripts/codebuild.sh
+++ scripts/codebuild.sh
@@ -1,21 +1,30 @@
-CODEBUILD_RESPONSE=$(aws codebuild start-build --project-name shop-storefront --environment-variables-override '[{"name":"TENANT_ID","value":"2"},{"name":"NEXT_PUBLIC_TENANT_NAME","value":"olcsos"},{"name":"NEXT_PUBLIC_SERVER_URL","value":"https://api.olcsos.shop.akosfi.com"}]')
+#!/bin/bash
 
+echo "Started building storefront image."
+
+CODEBUILD_RESPONSE=$(aws codebuild start-build --project-name shop-storefront --environment-variables-override '[{"name":"TENANT_ID","value":"2"},{"name":"NEXT_PUBLIC_TENANT_NAME","value":"olcsos"},{"name":"NEXT_PUBLIC_SERVER_URL","value":"https://api.olcsos.shop.akosfi.com"}]')
 CODEBUILD_BUILD_ID=$(echo $CODEBUILD_RESPONSE | jq '.build.id' | sed -e 's/^"//' -e 's/"$//')
 
-#CODEBUILD_BUILD_ID="shop-storefront:4ce34fc4-9d36-4bb1-8574-86f88426e420"
-echo $CODEBUILD_BUILD_ID
-#shop-storefront:4ce34fc4-9d36-4bb1-8574-86f88426e420
+echo "CodeBuild build ID: $CODEBUILD_BUILD_ID"
+
 while true
 do
-  echo "loop"
-  CURRENT_PHASE=$(aws codebuild batch-get-builds --ids $CODEBUILD_BUILD_ID | jq '.builds[0].currentPhase')
-  BUILD_STATUS=$(aws codebuild batch-get-builds --ids $CODEBUILD_BUILD_ID | jq '.builds[0].buildStatus')
-  echo $CURRENT_PHASE
-  echo $BUILD_STATUS
-  if [ $CURRENT_PHASE = "COMPLETED" ] && [ $BUILD_STATUS = "SUCCEEDED"]
+  echo "Checking for status."
+  CURRENT_PHASE=$(aws codebuild batch-get-builds --ids $CODEBUILD_BUILD_ID | jq '.builds[0].currentPhase' | sed -e 's/^"//' -e 's/"$//')
+  BUILD_STATUS=$(aws codebuild batch-get-builds --ids $CODEBUILD_BUILD_ID | jq '.builds[0].buildStatus' | sed -e 's/^"//' -e 's/"$//')
+
+  echo "CodeBuild build phase: $CURRENT_PHASE"
+  echo "CodeBuild build status: $BUILD_STATUS"
+
+  if [ "$CURRENT_PHASE" = "COMPLETED" ] && [ "$BUILD_STATUS" = "SUCCEEDED" ]
   then
-    echo "done"
+    echo "Finished building storefront image. (successful)"
+    break
+  elif [ "$CURRENT_PHASE" = "COMPLETED" ] && [ "$BUILD_STATUS" != "SUCCEEDED" ]
+  then
+    echo "Finished building storefront image. (unsuccessful)"
     break
   fi
+
   sleep 20
 done
